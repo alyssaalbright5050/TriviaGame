762ce6e218d32fef44b0abc44f7b854932ee0111
Alyssa Albright <alyssaalbright5050@gmail.com>
2019-10-23 21:22:02 -0500

    some code completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+$("#start").on("click", function() {
+    $("#start").remove();
+});
+
+var questions = [{
+    question: "Which dog is nicknamed 'The American Gentleman?",
+    answers: ["Cockapoo", "Boston Terrier", "American Bulldog", "American Foxhound"],
+    correctAnswer: "Boston Terrier",
+    //image: ,
+    question: "Which group does the Boston Terrier belong to, according to the American Kennel Club?",
+    answers: ["The Sporting Group", "The Working Group", "The Toy Group", "The Non-Sporting Group"],
+    correctAnswer: "The Non-Sporting Group",
+    //image: ,
+    question: "What year did the Boston Terrier originate?",
+    answers: ["1870", "1970", "1950", "1850"],
+    correctAnswer: "1850",
+    //image: ,
+    question: "Which was the first American breed to be recognized by the American Kennel Club?",
+    answers: ["Alasakan Malamute", "American Bulldog", "Boston Terrier", "American Foxhound"],
+    correctAnswer: "Boston Terrier",
+    //image: ,
+    question: "Where did the Boston Terrier originate?",
+    answers: ["Scotland", "United States", "Ireland", "England"],
+    correctAnswer: "United States",
+    //image:,
+    question: "What breed of dog is the cross-breed of an English Bulldog and a white English Terrier?",
+    answers: ["Bull Terrier", "Bulldog Terrier", "English Bulldog Terrier", "Boston Terrier"],
+    correctAnswer: "Boston Terrier",
+    //image: ,
+    question: "In what year did the American Kennel Club admit the Boston Terrier?",
+    answers: ["1893", "1975", "1875", "1993"],
+    correctAnswer: "1893",
+    //image: ,
+    question: "What is the average life expectancy of a Boston Terrier?",
+    answers: ["9-13 years", "10-14 years", "11-15 years", "12-16 years"],
+    correctAnswer: "10-14 years"
+    //image:,
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
